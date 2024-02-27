ea3da1827c16bd4e44e6b6b913c6dba0a3ce4696
Tutku Ince <tutku.ince@outlook.com>
2024-02-27 15:05:46 +0300

    new project init

---
+++ kafka-tutorial/src/test/java/io/tutkuince/kafkatutorial/KafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tutkuince.kafkatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
